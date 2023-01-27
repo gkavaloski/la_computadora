0be60a06061a9ca36e3d0a138438e312b485a618
gkavaloski <gkavaloski@natomascharter.org>
2023-01-26 21:59:19 -0800

    added to spanishdict

--- espanol.py
+++ espanol.py
@@ -1,5 +1,15 @@
 import random
 import spanishdicts
+import sys
+
+# If you want to practice just most current,
+# add 'recent' to your cli argument.
+
+try:
+    type_of_challenge = sys.argv[1]
+except:
+    type_of_challenge = 0
+
 
 
 print("How many phrases would you like to practice?")
@@ -9,27 +19,53 @@ correct_answers = 0
 wrong_answers = 0
 total_score = 0
 
-for i in range(amount_of_phrases):
-    spanish, anglo = random.choice(list(spanishdicts.anglo_spanish.items()))
-    print("\n" + anglo + "\n")
-    answer = input("~~: ")
-    if answer == spanish:
-        print("\n" + "." * 26)
-        print("Success!!!")
-        print("." * 26)
-        correct_answers = correct_answers + 1
-    else:
-        print("\n" + "X" * 26)
-        print("Nope. Try: " + spanish)
-        print("X" * 26)
-        wrong_answers = wrong_answers + 1
+if type_of_challenge == "recent":
+    print("HARD MODE ENGAGED!")
+    for i in range(amount_of_phrases):
+        spanish, anglo = random.choice(list(spanishdicts.recent_phrases.items()))
+        print("\n" + anglo + "\n")
+        answer = input("~~: ")
+        if answer == spanish:
+            print("\n" + "." * 26)
+            print("Success!!!")
+            print("." * 26)
+            correct_answers = correct_answers + 1
+        else:
+            print("\n" + "X" * 26)
+            print("No: " + spanish)
+            print("X" * 26)
+            wrong_answers = wrong_answers + 1
 
-if wrong_answers == 0:
-    total_score = 100
-elif correct_answers == 0:
-    total_score = 0
+    if wrong_answers == 0:
+        total_score = 100
+    elif correct_answers == 0:
+        total_score = 0
+    else:
+        total_score = round((correct_answers / amount_of_phrases) * 100, 2)
+
 else:
-    total_score = round((correct_answers / amount_of_phrases) * 100, 2)
+    for i in range(amount_of_phrases):
+        print("Bienvenidos, tu pepino es muy pequeno!")
+        spanish, anglo = random.choice(list(spanishdicts.anglo_spanish.items()))
+        print("\n" + anglo + "\n")
+        answer = input("~~: ")
+        if answer == spanish:
+            print("\n" + "." * 26)
+            print("Success!!!")
+            print("." * 26)
+            correct_answers = correct_answers + 1
+        else:
+            print("\n" + "X" * 26)
+            print("No: " + spanish)
+            print("X" * 26)
+            wrong_answers = wrong_answers + 1
+
+    if wrong_answers == 0:
+        total_score = 100
+    elif correct_answers == 0:
+        total_score = 0
+    else:
+        total_score = round((correct_answers / amount_of_phrases) * 100, 2)
 
 print("\n End of game.")
 print(str(correct_answers) + " correct, and " + str(wrong_answers) + " wrong.")
